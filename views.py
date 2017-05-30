5281675db464b8c22a8bfb920044a02f6d6e184c
williamh890 <wbhorn@alaska.edu>
2017-05-29 18:07:42 -0800

    Basic functionality achieved
    Need to add support for the different types of questions, as well as randomizing the question order along with the multiple choice order

--- views.py
+++ views.py
@@ -1,11 +1,49 @@
-from flask import request, render_template
+from flask import request, render_template, url_for
 from app import app
+import json
 
 @app.route('/')
-def index():
+def start():
     return render_template('index.html')
 
-@app.route('/sum')
-@app.route('/sum/<int:x>/<int:y>')
-def sum(x=None, y=None):
-    return render_template('sum.html', x=x, y=y)
+class Answer:
+    def __init__(self, qText, submitied, correct):
+        self.question_text = qText
+        self.submitied_answer = submitied
+        self.isCorrect = correct
+
+
+@app.route('/questions', methods=['GET', 'POST'])
+def questions():
+    #open the json file
+    with open("AandPQuestions.json") as questions:
+        quiz=json.load(questions)['quiz']
+
+        if request.method == 'GET':
+                #pass the json dicitonary to the template
+                return render_template("question.html", quiz=quiz)
+
+        #the information is being submitied
+        if request.method == 'POST':
+            submissions = []
+            total_correct = 0
+            isCorrect = False
+            #look through the answers
+
+            for question, submitted_answer in request.form.items():
+
+                if quiz[question]["answer"] == submitted_answer:
+                    isCorrect = True
+                    total_correct += 1
+                else:
+                    isCorrect = False
+                submissions.append(Answer(question, submitted_answer, isCorrect))
+
+            #return "%f, %f, %f" % (total_correct, len(quiz), (total_correct / len(quiz)) * 100)
+            return render_template("submission.html", submissions=submissions,
+                                                      total_correct=total_correct,
+                                                      length=len(quiz),
+                                                      percentage=((total_correct / len(quiz) * 100)))
+
+
+app.secret_key = 'A0Zr98j/3yX R~XHH!jmN]LWX/,?RT'
